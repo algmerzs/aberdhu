0f37d03349b0e5d53076dbdabaff68794c237552
Mariana <maria3410zi@gmail.com>
2021-06-11 18:14:14 -0500

    istalación de vue en carpeta client

---
+++ client/src/views/News.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div id="news"></div>
+  </div>
+</template>
+
+<script>
+const url =
+  "https://api.polygon.io/v2/reference/news?apiKey=KjushOl8xFajibrTsdzJkU8SGYlb13OE";
+const listNews = document.querySelector("#news");
+const getInfo = async () => {
+  let res = await fetch(url);
+  let newsInJson = await res.json();
+  let news = await newsInJson.results;
+
+  console.log(news);
+
+  // listNews.innerHTML += news
+  //   .map(
+  //     (n) =>
+  //       `<div> <h3> ${n.title} </h3> <p> ${n.description} </p> <sub> ${n.published_utc} </sub> </div>`
+  //   )
+  //   .join("");
+};
+getInfo();
+</script>
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const morgan = require("morgan");
-const path = require("path");
 
 const app = express();
 
@@ -12,12 +11,6 @@ app.set("json spaces", 2);
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-//static files
-app.use(express.static(path.join(__dirname, '/public')));
-
- //routes
-app.use(require("./routes/routes"));
 
 // server
 app.listen(app.get("port"), () =>
