5cc3f0bb00e31b71d74768c5529e8cb4e479068c
Mariana <maria3410zi@gmail.com>
2021-09-11 12:36:26 -0500

    paginación noticias

--- public/js/getNews.js
+++ public/js/getNews.js
@@ -4,9 +4,8 @@ const newsList = new Vue({
         news: []
     },
     async mounted() {
-        
-        let res = await fetch(`https://api.polygon.io/v2/reference/news?${"limit=4"}&apiKey=dDrG9Sy4A8u30T0WffgfzqJfEl70Vbfo`)
 
+        let res = await fetch(`https://api.polygon.io/v2/reference/news?${"limit=4"}&apiKey=dDrG9Sy4A8u30T0WffgfzqJfEl70Vbfo`)
         let newsInJson = await res.json();
         let news = await newsInJson.results;
         this.news = news;
@@ -16,14 +15,44 @@ const newsList = new Vue({
 const newsList2 = new Vue({
     el: '#news2',
     data: {
-        news: []
+        news: [],
+        baseURL: "https://api.polygon.io/v2/reference/news?limit=50&order=descending&sort=published_utc&ticker=AAPL&published_utc.gte=2021-04-26&apiKey=dDrG9Sy4A8u30T0WffgfzqJfEl70Vbfo",
+        page: 1,
+        perPage: 10,
+        pages: []
     },
-    async mounted() {
-        
-        let res = await fetch(`https://api.polygon.io/v2/reference/news?${"limit=10"}&apiKey=dDrG9Sy4A8u30T0WffgfzqJfEl70Vbfo`)
-
-        let newsInJson = await res.json();
-        let news = await newsInJson.results;
-        this.news = news;
+    created() {
+        this.getNews();
+    },
+    methods: {
+        async getNews() {
+            let res = await fetch(this.baseURL);
+            let newsInJson = await res.json();
+            let news = await newsInJson.results;
+            this.news = news;
+        },
+        paginate(news) {
+            let page = this.page;
+            let perPage = this.perPage;
+            let from = (page * perPage) - perPage;
+            let to = (page * perPage);
+            return news.slice(from, to);
+        },
+        setNews() {
+            let nPages = Math.ceil(this.news.length / this.perPage);
+            for (i = 1; i <= nPages; i++) {
+                this.pages.push(i);
+            }
+        }
+    },
+    computed: {
+        displayedNews() {
+            return this.paginate(this.news);
+        }
+    },
+    watch: {
+        news() {
+            this.setNews();
+        }
     }
 });
