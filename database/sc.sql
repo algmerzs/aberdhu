8f1dee1567ab78e97f036b4f6d368ed154c64c99
Mariana <maria3410zi@gmail.com>
2021-10-13 17:28:38 -0500

    añadir modulos, preparar config para correos

--- database/sc.sql
+++ database/sc.sql
@@ -1,5 +1,8 @@
 CREATE TABLE indicators(
     id INT(11) NOT NULL,
     symbol VARCHAR(10) NOT NULL,
-    
-)
+    current_price DECIMAL(15,15) NOT NULL,
+    username INT(11),
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT fk_user FOREIGN KEY(username) REFERENCES users(username)
+);
