d22ced86899c2e9d4da75558da0ba54fb2c903c5
sarroyave27 <snarroyave@misena.edu.co>
2021-10-14 12:32:05 -0500

    responsive noticias

--- public/js/getNews.js
+++ public/js/getNews.js
@@ -5,7 +5,7 @@ const newsList = new Vue({
     },
     async mounted() {
 
-        let res = await fetch(`https://api.polygon.io/v2/reference/news?${"limit=4"}&apiKey=dDrG9Sy4A8u30T0WffgfzqJfEl70Vbfo`)
+        let res = await fetch(`https://api.polygon.io/v2/reference/news?${"limit=6"}&apiKey=dDrG9Sy4A8u30T0WffgfzqJfEl70Vbfo`)
         let newsInJson = await res.json();
         let news = await newsInJson.results;
         this.news = news;
