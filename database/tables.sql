153be85ea1fcfce3654a17424991d9528dac6add
Mariana <maria3410zi@gmail.com>
2021-10-15 22:36:27 -0500

    enviar correo

--- database/tables.sql
+++ database/tables.sql
@@ -4,7 +4,9 @@ CREATE TABLE users(
     id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(16) NOT NULL,
     password VARCHAR(60) NOT NULL,
-    email VARCHAR(80) NOT NULL
+    email VARCHAR(80) NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_at TIMESTAMP NOT NULL,
 );
 
 -- tabla de criptomonedas
@@ -13,6 +15,7 @@ CREATE TABLE indicators(
     symbol VARCHAR(10) NOT NULL,
     current_price FLOAT(15,15) NOT NULL,
     indi_username INT(11) NOT NULL,
-    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+    created_at TIMESTAMP NOT NULL,
     CONSTRAINT fk_indic FOREIGN KEY(indi_username) REFERENCES users(id)
 );
